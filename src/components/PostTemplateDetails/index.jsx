d95050e868a968fe335a128810ab949a2edb5060
Nabil-Fareed Alikhan <nabil@happykhan.com>
2020-03-23 23:30:42 +0000

    footer

--- src/components/PostTemplateDetails/index.jsx
+++ src/components/PostTemplateDetails/index.jsx
@@ -64,13 +64,7 @@ class PostTemplateDetails extends React.Component {
             <hr />
             <p className="post-single__footer-text">
               {subtitle}
-              <a
-                href={`https://twitter.com/${author.twitter}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <br /> <strong>{author.name}</strong> on Twitter
-              </a>
+                <br /> <strong>{author.name}</strong>
             </p>
             {commentsBlock}
           </div>
