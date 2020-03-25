9eef706458967eed2918e92103b03aa7c3ed7e89
Nabil-Fareed Alikhan <nabil@happykhan.com>
2020-03-25 12:44:53 +0000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-158aafffdb8d343014f7.js"
+    "url": "webpack-runtime-f340fcb825e597d920e0.js"
   },
   {
     "url": "app-f97e0510293a1073f913.js"
@@ -36,11 +36,11 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "babe78cd0bc97ccd2e43fe9c7d5c5850"
+    "revision": "a854f251ed39ab8e321a2e3264af88dd"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9cded11ffe76cf6fca04b6ebfd007c4f"
+    "revision": "48a30975d1e4f05c73c5784e23837c76"
   },
   {
     "url": "2.462b440b4b1b5df626b3.css"
